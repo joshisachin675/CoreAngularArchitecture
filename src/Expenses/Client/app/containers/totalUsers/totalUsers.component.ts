5759d4086f931766071972f2818266d21812d087
Sachin Joshi <sachin.joshi@onebcg.com>
2018-11-22 16:18:31 +0530

    First commit

---
+++ src/Expenses/Client/app/containers/totalUsers/totalUsers.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'totalUsers',
+    templateUrl: './totalUsers.component.html'
+})
+export class TotalUsersComponent {
+}
---
+++ src/Expenses/Client/app/resolvers/totalUser.ts
@@ -0,0 +1,14 @@
+﻿import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { TotalUserService } from '../shared/totalUser.service';
+import { IUser } from "../models/ITotalUser";
+
+@Injectable()
+export class TotalUserResolve implements Resolve<TotalUserService> {
+
+    constructor(private totalUserService: TotalUserService) { }
+
+    resolve(route: ActivatedRouteSnapshot) {
+        return this.totalUserService.getOne(route.paramMap.get('id'));
+    }
+}
